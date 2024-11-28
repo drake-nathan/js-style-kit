21a3674488eabe3f8d423068adfefa443a210ca8
Nathan Drake <nathandrakedrums@gmail.com>
2024-11-28 11:24:42 -0600

    setup prettier eslint

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,8 @@
 import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import perfectionist from "eslint-plugin-perfectionist";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -11,8 +11,9 @@ import onlyWarn from "eslint-plugin-only-warn";
  * */
 export const config = [
   js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  perfectionist.configs["recommended-natural"],
   {
     plugins: {
       turbo: turboPlugin,
