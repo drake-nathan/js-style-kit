b97d034e61392a3d79977d42daf457a874a0c619
Nathan Drake <nathandrakedrums@gmail.com>
2025-03-11 17:30:42 -0500

    docs(mintlify): Trash docusaurus, add mintlify, remove apps/ dir

--- apps/docs/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import { eslintConfig } from "js-style-kit";
-
-export default eslintConfig({
-  functionStyle: "off",
-  ignores: [".docusaurus"],
-  react: true,
-  typescript: "./tsconfig.eslint.json",
-});
