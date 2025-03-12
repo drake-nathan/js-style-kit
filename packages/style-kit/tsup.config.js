53c9ec75f750908f3f033c9ab948b7134b6eb189
Nathan Drake <nathandrakedrums@gmail.com>
2025-03-12 14:25:00 -0500

    feat(cli): init

--- packages/style-kit/tsup.config.js
+++ packages/style-kit/tsup.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   clean: true,
   dts: true,
-  entryPoints: ["src/**/*"],
+  entryPoints: ["src/**/*", "bin/**/*"],
   format: ["esm"],
   outDir: "dist",
   sourcemap: true,
