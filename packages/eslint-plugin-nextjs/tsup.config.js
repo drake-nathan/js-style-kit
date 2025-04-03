2036278c128aa372ac459120863945991e2f33b3
Nathan Drake <nathandrakedrums@gmail.com>
2025-04-02 20:58:04 -0500

    update packages, remove cjs from eslint-plugin-nextjs

--- packages/eslint-plugin-nextjs/tsup.config.js
+++ packages/eslint-plugin-nextjs/tsup.config.js
@@ -5,7 +5,7 @@ export default defineConfig([
     clean: true,
     dts: true,
     entryPoints: ["src/**/*", "!test/**/*"],
-    format: ["esm", "cjs"],
+    format: ["esm"],
     outDir: "dist",
     sourcemap: true,
     splitting: false,
