18d24d11d4360b4f6152f8a6ef95ea3c266c16cc
Nathan Drake <nathandrakedrums@gmail.com>
2025-04-05 19:21:21 -0500

    try adding cjs back

--- packages/eslint-plugin-nextjs/tsup.config.js
+++ packages/eslint-plugin-nextjs/tsup.config.js
@@ -5,7 +5,7 @@ export default defineConfig([
     clean: true,
     dts: true,
     entryPoints: ["src/**/*", "!test/**/*"],
-    format: ["esm"],
+    format: ["esm", "cjs"],
     outDir: "dist",
     sourcemap: true,
     splitting: false,
